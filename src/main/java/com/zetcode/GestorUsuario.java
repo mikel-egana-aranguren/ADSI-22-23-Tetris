d9e81c5a94b24fdcb34d9a1d3b69dced78c525b9
un4rch <81522320+un4rch@users.noreply.github.com>
2022-12-14 13:50:38 +0100

    Create GestorUsuario.java

---
+++ src/main/java/com/zetcode/GestorUsuario.java
@@ -0,0 +1,30 @@
+package com.zetcode;
+
+public class GestorUsuario {
+	private static GestorUsuario miGestorUsuario = null;
+	private Usuario usuarioActual = null;
+	
+	private GestorUsuario() {
+		
+	}
+	
+	public static GestorUsuario getGestorUsuario() {
+		if (miGestorUsuario == null) {
+			miGestorUsuario = new GestorUsuario();
+		}
+		return miGestorUsuario;
+	}
+	
+	public Usuario obtenerUsuarioActual() {
+		return this.usuarioActual;
+	}
+	
+	public String getNombreUsuario(Usuario pUsuario) {
+		return pUsuario.getNombre();
+	}
+	
+	public Partida obtenerPartidaUsuario(Usuario pUsuario) {
+		return pUsuario.obtenerPartida();
+	}
+
+}
