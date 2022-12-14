508603bb8986b77a68e17f00d77571982bf100f2
un4rch <81522320+un4rch@users.noreply.github.com>
2022-12-14 13:57:52 +0100

    Update Partida.java

--- src/main/java/com/zetcode/Partida.java
+++ src/main/java/com/zetcode/Partida.java
@@ -10,6 +10,10 @@ public class Partida {
     private int puntos;
     private int idPartida;
     private ArrayList<Ficha> listaFichas;
+    
+    public Partida() {
+    
+    }
 
     private int fichasColocadas() {
         // TODO
@@ -42,9 +46,8 @@ public class Partida {
         // TODO
     }
     
-    private int obtenerPuntos() {
-        // TODO
-        return 0;
+    public int obtenerPuntos() {
+        return this.puntos;
     }
 
     private ArrayList<Premio> obtenerPremios() {
@@ -52,25 +55,42 @@ public class Partida {
         return null;
     }
 
-    private int obtenerId() {
-        // TODO
-        return 0;
+    prublic int obtenerId() {
+        return this.idPartida;
     }
 
-    private String obtenerEstadoTablero() {
-        // TODO
-        return "";
-    }
+    public JSONObject obtenerEstadoTablero() {
+		JSONObject[] arrayDatosFichas = {};
+		for (Ficha pFicha : listaFichas) {
+			JSONObject datosFicha = pFicha.obtenerDatosFicha();
+			arrayDatosFichas[arrayDatosFichas.length] = datosFicha;
+		}
+		JSONObject datosFichas = new JSONObject();
+		datosFichas.put("estadoTablero", arrayDatosFichas);
+		return datosFichas;
+		
+	}
 
-    private void setPuntosPartida(int i1) {
-        // TODO
-    }
+    public void setPuntosPartida(int pPuntos) {
+		this.puntos = pPuntos;
+	}
     
-    private void setIdPartida(int i1) {
-        // TODO
-    }
+    public void setIdPartida(int pIdPartida) {
+		this.idPartida = pIdPartida;
+	}
 
     private void anadirListaFichas(ArrayList<Ficha> listaFichas) {
         // TODO
     }
+    
+    public void setEstadoTablero(JSONObject pEstadoTableroJson) {
+		JSONObject[] arrayFichasJson = (JSONObject[])pEstadoTableroJson.get("estadoTablero");
+		for (JSONObject fichaJson : arrayFichasJson) {
+			int posX = fichaJson.getInt("posX");
+			int posY = fichaJson.getInt("posY");
+			String color = fichaJson.getString("color"); 
+			Ficha ficha = new Ficha(posX, posY, color);
+			listaFichas.add(ficha);
+		}
+	}
 }
