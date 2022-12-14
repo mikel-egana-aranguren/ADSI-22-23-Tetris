19d60e1fcd5b4ba15809c331147b548aaccf321c
un4rch <81522320+un4rch@users.noreply.github.com>
2022-12-14 13:45:47 +0100

    Update Ficha.java

--- src/main/java/com/zetcode/Ficha.java
+++ src/main/java/com/zetcode/Ficha.java
@@ -1,16 +1,29 @@
 package com.zetcode;
 
+import org.json.JSONObject;
+
 public class Ficha {
     private int posX;
     private int posY;
     private String color;
-
-    private void setDatosFicha(int posX, int posY, String color) {
-        // TODO
+    
+    public Ficha(int pPosX, int pPosY, String pColor) {
+    	this.posX = pPosX;
+        this.posY = pPosY;
+        this.color = pColor;
+    }
+    
+    public void setDatosFicha(int pPosX, int pPosY, String pColor) {
+        this.posX = pPosX;
+        this.posY = pPosY;
+        this.color = pColor;
     }
 
-    private String obtenerDatosFicha() {
-        // TODO
-        return "";
+    public JSONObject obtenerDatosFicha() {
+    	JSONObject datosFicha = new JSONObject();
+    	datosFicha.put("posX", this.posX);
+    	datosFicha.put("posY", this.posY);
+    	datosFicha.put("color", this.color);
+        return datosFicha;
     }
 }
