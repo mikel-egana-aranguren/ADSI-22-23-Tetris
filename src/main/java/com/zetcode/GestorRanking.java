a0d4ed7ff3e16cdf26f6e4a4f6fb7cedcef2552e
amartinl89 <113670011+amartinl89@users.noreply.github.com>
2022-12-18 04:55:50 +0100

    Update GestorRanking.java

--- src/main/java/com/zetcode/GestorRanking.java
+++ src/main/java/com/zetcode/GestorRanking.java
@@ -34,7 +34,7 @@ public class GestorRanking {
     			+" FROM "
     			+"(SELECT * "
     			+ "FROM Ranking "
-    			+ "ORDER BY Puntos Desc, NivelDificultad, FechaHora) "+
+    			+ "ORDER BY Puntos Desc, NivelDificultad Desc, FechaHora) "+
     			"WHERE NivelDificultad = '%s'",difi));
     	while(res.next()) {
 	    	JSONObject jsu=new JSONObject();
@@ -54,7 +54,7 @@ public class GestorRanking {
     			+ "FROM "
     			+ "(SELECT * "
     			+ "FROM Ranking "
-    			+ "ORDER BY Puntos Desc, NivelDificultad, FechaHora) "
+    			+ "ORDER BY Puntos Desc, NivelDificultad Desc, FechaHora) "
     			+ "WHERE NombreUsuario = '%s'",nombre));
     	while(res.next()) {
 	    	JSONObject jsu=new JSONObject();
@@ -72,7 +72,7 @@ public class GestorRanking {
     			+ "FROM "
     			+ "(SELECT * "
     			+ "FROM Ranking "
-    			+ "ORDER BY Puntos Desc, NivelDificultad, FechaHora) "
+    			+ "ORDER BY Puntos Desc, NivelDificultad Desc, FechaHora) "
     			+ "WHERE NombreUsuario = '%s'"
     			+ " AND NivelDificultad = '%s'", nombre,difi));
     	while(res.next()) {
