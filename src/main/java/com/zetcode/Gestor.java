cae40338d7d0276527fb75ce72b95aeb2e3a6e52
Calcoph <34724728+Calcoph@users.noreply.github.com>
2022-12-18 17:49:09 +0100

    arreglados todos los errores de compilación

--- src/main/java/com/zetcode/Gestor.java
+++ src/main/java/com/zetcode/Gestor.java
@@ -13,10 +13,8 @@ import java.awt.Toolkit;
 import org.json.JSONException;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.lang.StackWalker.Option;
-import java.util.ArrayList;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -126,48 +124,9 @@ public class Gestor {
     }
     
 
-    public boolean comprobarDatosRegistro(String usuario, String pEmail, String pwd1, String pwd2) {
-    	JOptionPane option = new JOptionPane();
-    	if (usuario.length()!=0 || pEmail.length()!=0 || pwd1.length()!=0 || pwd2.length()!=0) {
-    		if (usuario.length()>2) {
-        		if (pwd1.length()>3 && pwd2.length()>3) {
-        			if (pwd1.equals(pwd2)== true) {
-        				Pattern emailPat = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-        				Matcher matcher = emailPat.matcher(pEmail);
-        				if (matcher.find()==true) {
-        					GestorUsuario GU = new GestorUsuario();
-        					if (GU.existeUsuario(usuario) == false && GU.existeEmail(pEmail) == false) {
-        						option.showMessageDialog(null, "Te has registrado correctamente", "ÉXITO", JOptionPane.INFORMATION_MESSAGE);
-        						return true;
-        					} else {
-        						option.showMessageDialog(null, "El usuario introducido ya existe", "ERROR",JOptionPane.ERROR_MESSAGE);
-        						return false;
-        					}
-        				} else {
-        					option.showMessageDialog(null, "El email introducido no es válido", "ERROR",JOptionPane.ERROR_MESSAGE);
-        					return false;
-        				}
-        			} else {
-        				option.showMessageDialog(null, "Las contraseñas no son iguales", "ERROR",JOptionPane.ERROR_MESSAGE);
-        				return false;
-        			} 
-    			} else {
-    				option.showMessageDialog(null, "La contraseña introducida debe contener al menos 4 caracteres", "ERROR",JOptionPane.ERROR_MESSAGE);
-    				return false;
-    			}
-    		} else {
-    			option.showMessageDialog(null, "El nombre de usuario debe contener al menos 3 caracteres", "ERROR",JOptionPane.ERROR_MESSAGE);
-    			return false;
-    		}
-		} else {
-			option.showMessageDialog(null, "Por favor, rellene todos los campos", "ERROR",JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-    }
-    
     public boolean comprobarDatosCambiarContraseña(String usuario, String pwdOld, String pwd1, String pwd2) {
     	JOptionPane option = new JOptionPane();
-    	GestorUsuario GU = new GestorUsuario();
+    	GestorUsuario GU = GestorUsuario.getGestorUsuario();
     	if (usuario !="admin") {
     		if (usuario.length()!=0 || pwdOld.length()!=0 || pwd1.length()!=0 || pwd2.length()!=0) {
         		if (GU.existeUsuario(usuario)) {
@@ -202,106 +161,11 @@ public class Gestor {
 		}
     }
 
-    public int registrarse(String usu, String email, String pwd1, String pwd2) {
-    	GestorUsuario GU = new GestorUsuario();
-    	if (comprobarDatosRegistro(usu, email, pwd1, pwd2)) {
-    		GU.registrarse(usu, email, pwd1);
-    		return 1;
-		} else {
-			return 0;
-		}
-    }
-
-    public boolean identificarse(String usu, String pwd) {
-    	JOptionPane option = new JOptionPane();
-    	GestorUsuario GU = new GestorUsuario();
-        if (usu.length()!=0 || pwd.length()!=0) {
-        	if (GU.existeUsuario(usu, pwd)) {
-            	return true;
-            } else {
-    			option.showMessageDialog(null, "Las credenciales introducidas no son correctas \nVuelve a intentarlo", "ERROR",JOptionPane.ERROR_MESSAGE);
-    			return false;
-    		}
-		} else {
-			option.showMessageDialog(null, "Por favor, rellene todos los campos", "ERROR",JOptionPane.ERROR_MESSAGE);
-			return false;
-		}
-    }
-
-    private void recuperar(String nombreUsuario, String pwd, String destinatario) {
-    	String asunto = "Contraseña TETRIX";
-		String cuerpo = "La contraseña correspondiente al usuario " + nombreUsuario + "es: " + pwd;
-
-		String remitente = "adsitetrix@gmail.com";
-		String clave = "tetrixsa22";
-
-		Properties props = System.getProperties();
-		props.setProperty("mail.smtp.host", "smtp.gmail.com"); // Servidor SMTP de Google
-		//props.setProperty("mail.smtp.user", remitente); // Correo electronico desde donde se mandara el mensaje
-		//props.setProperty("mail.smtp.clave", clave); // La clave de la cuenta
-		props.setProperty("mail.smtp.auth", "true"); // Usar autenticación mediante usuario y clave
-		props.setProperty("mail.smtp.starttls.enable", "true"); // Para conectar de manera segura al servidor SMTP
-		props.setProperty("mail.smtp.port", "587"); // El puerto SMTP seguro de Google
-		
-		javax.mail.Session session = javax.mail.Session.getInstance(props,new javax.mail.Authenticator() {
-			public PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(remitente, clave);
-			}
-		});
-		
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(remitente));
-			message.addRecipients(Message.RecipientType.TO, destinatario);
-			message.setSubject(asunto);
-			message.setText(cuerpo);
-			Transport transport = session.getTransport("smtp");
-			transport.connect("smtp.gmail.com", remitente, clave);
-			transport.sendMessage(message, message.getAllRecipients());
-			transport.close();
-		} catch (MessagingException me) {
-			me.printStackTrace();
-		}
-	}
-
-	public void enviarEmail (String texto) {
-		GestorUsuario GU = new GestorUsuario();
-		String[] credenciales = GU.obtenerDatos(texto);
-		JOptionPane option = new JOptionPane();
-		if (credenciales[0]!=null) {
-			recuperar(credenciales[0], credenciales[1], credenciales[2]);
-			option.showMessageDialog(null, "Se han enviado los datos a su email", "DATOS ENVIADOS", JOptionPane.INFORMATION_MESSAGE);
-		} else {
-			option.showMessageDialog(null, "No se han podido enviar los datos a su email", "DATOS NO ENVIADOS", JOptionPane.ERROR_MESSAGE);
-		}
-	}
-
-
     private boolean comprobar(String usu, String pwdVieja, String pwd1, String pwd2) {
         // TODO
         return false;
     }
 
-    public void cambiar(String usu, String pwdOld, String pwd1, String pwd2) {
-        GestorUsuario GU = GestorUsuario.getGestorUsuario();
-        if (comprobarDatosCambiarContraseña(usu, pwdOld, pwd1, pwd2)) {
-			GU.cambiarContraseña(usu, pwd1);
-		}
-    }
-
- 	public void eliminarUsuario(String usu) {
-    	JOptionPane option = new JOptionPane();
-        GestorUsuario GU = GestorUsuario.getGestorUsuario();
-        if (GU.existeUsuario(usu)) {
-        	int resp = JOptionPane.showConfirmDialog(null, "Estas seguro de que quieres eliminar al usuario: " + usu, "ATENCIÓN", JOptionPane.YES_NO_OPTION);
-        	if (resp == 0) {
-				System.out.println("Usuario eliminado");
-				//GU.eliminarUsuario(usu);
-			}
-		} else {
-			option.showMessageDialog(null, "El usuario introducido no existe", "ERROR",JOptionPane.ERROR_MESSAGE);
-		}
- 	}
     public boolean comprobarDatosRegistro(String usuario, String pEmail, String pwd1, String pwd2) {
     	JOptionPane option = new JOptionPane();
     	if (usuario.length()!=0 || pEmail.length()!=0 || pwd1.length()!=0 || pwd2.length()!=0) {
@@ -311,7 +175,7 @@ public class Gestor {
         				Pattern emailPat = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+"[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         				Matcher matcher = emailPat.matcher(pEmail);
         				if (matcher.find()==true) {
-        					GestorUsuario GU = new GestorUsuario();
+        					GestorUsuario GU = GestorUsuario.getGestorUsuario();
         					if (GU.existeUsuario(usuario) == false && GU.existeEmail(pEmail) == false) {
         						option.showMessageDialog(null, "Te has registrado correctamente", "EXITO", JOptionPane.INFORMATION_MESSAGE);
         						return true;
