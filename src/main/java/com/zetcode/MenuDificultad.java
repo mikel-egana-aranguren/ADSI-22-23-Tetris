ab36710964f06c3dee4ec47926b2661b1f0b697b
Jorbius <borjapecharroman@gmail.com>
2022-12-18 21:53:28 +0100

    Update MenuDificultad.java

--- src/main/java/com/zetcode/MenuDificultad.java
+++ src/main/java/com/zetcode/MenuDificultad.java
@@ -57,6 +57,7 @@ public class MenuDificultad extends JFrame {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 271);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(153, 153, 153));
 		contentPane.setForeground(new Color(153, 153, 102));
